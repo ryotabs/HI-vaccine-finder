308ee3ed1a99913c87646bacd795e3745399ce64
johnbigornia <jbigornia15@gmail.com>
2021-04-23 02:25:08 -1000

    Basic landing page, also cleaned up unused tempate code.

--- app/imports/ui/components/Footer.jsx
+++ app/imports/ui/components/Footer.jsx
@@ -3,10 +3,9 @@ import React from 'react';
 /** The Footer appears at the bottom of every page. Rendered by the App Layout component. */
 class Footer extends React.Component {
   render() {
-    const divStyle = { paddingTop: '15px' };
     return (
       <footer>
-        <div style={divStyle} className="ui center aligned container">
+        <div className="ui center aligned container">
           <hr />
               Department of Information and Computer Sciences <br />
               University of Hawaii<br />
--- app/imports/ui/pages/Landing.jsx
+++ app/imports/ui/pages/Landing.jsx
@@ -1,22 +1,20 @@
 import React from 'react';
-import { Grid, Image } from 'semantic-ui-react';
+import { Container, Button } from 'semantic-ui-react';
+import { NavLink } from 'react-router-dom';
 
 /** A simple static component to render some text for the landing page. */
 class Landing extends React.Component {
   render() {
     return (
-      <Grid id='landing-page' verticalAlign='middle' textAlign='center' container>
-
-        <Grid.Column width={4}>
-          <Image size='small' circular src="/images/meteor-logo.png"/>
-        </Grid.Column>
-
-        <Grid.Column width={8}>
-          <h1>Welcome to this template</h1>
-          <p>Now get to work and modify this app!</p>
-        </Grid.Column>
-
-      </Grid>
+      <div>
+        <Container>
+          <div className="landing-text-style">
+            <h1 className='header-landing'>Vaccine Finder</h1>
+            <p>Welcome to HI vaccine finder, the fastest and easiest way to find a legible vaccine!</p>
+            <Button size='huge' color='green' as={NavLink}exact to="/">Get Started</Button>
+          </div>
+        </Container>
+      </div>
     );
   }
 }
