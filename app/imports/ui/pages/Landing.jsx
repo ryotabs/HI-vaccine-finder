8fc57df7f36e2649cf31c7d2d3bb767c997e3d9b
James Bennett <46543890+Bennettjames649@users.noreply.github.com>
2021-04-30 16:18:38 -1000

    Update Landing.jsx
    Now button now redirects conditionally depending on if the user is logged in

--- app/imports/ui/pages/Landing.jsx
+++ app/imports/ui/pages/Landing.jsx
@@ -1,21 +1,56 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Container, Button } from 'semantic-ui-react';
 import { NavLink } from 'react-router-dom';
+import { Meteor } from 'meteor/meteor';
+
+class Landing extends React.Component {
+
+  constructor(props) {
+    super(props)
+  this.state = {
+      isLoggedIn: false
+  }
+}
+  componentDidMount() {
+    if (Meteor.user()) {
+      this.setState({
+        isLoggedIn: true
+      })
+    } else {
+      this.setState({
+        isLoggedIn: false
+      })
+
+    }
+  }
 
 /** A simple static component to render some text for the landing page. */
-class Landing extends React.Component {
   render() {
-    return (
-      <div>
-        <Container>
-          <div className="landing-text-style">
-            <h1 className='header-landing'>Vaccine Finder</h1>
-            <p>Welcome to HI vaccine finder, the fastest and easiest way to find a legible vaccine!</p>
-            <Button size='huge' color='green' as={NavLink}exact to="/signin">Get Started</Button>
+    if (this.state.isLoggedIn) {
+      return (
+          <div>
+            <Container>
+              <div className="landing-text-style">
+                <h1 className='header-landing'>Vaccine Finder</h1>
+                <p>Welcome to HI vaccine finder, the fastest and easiest way to find a legible vaccine!</p>
+                <Button size='huge' color='green' as={NavLink} exact to="/vaccine-form">Get Started</Button>
+              </div>
+            </Container>
           </div>
-        </Container>
-      </div>
-    );
+      );
+    } else {
+      return (
+          <div>
+            <Container>
+              <div className="landing-text-style">
+                <h1 className='header-landing'>Vaccine Finder</h1>
+                <p>Welcome to HI vaccine finder, the fastest and easiest way to find a legible vaccine!</p>
+                <Button size='huge' color='green' as={NavLink} exact to="/signin">Get Started</Button>
+              </div>
+            </Container>
+          </div>
+      );
+    }
   }
 }
 
